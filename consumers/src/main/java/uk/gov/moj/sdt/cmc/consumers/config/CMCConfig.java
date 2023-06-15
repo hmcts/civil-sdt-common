fd0d90d25ff48792238ec2854ea1f4d5b7123a7b
Kiran Yenigala <kiran.yenigala@hmcts.net>
2023-06-15 21:54:53 +0100

    Common library classes

--- consumers/src/main/java/uk/gov/moj/sdt/cmc/consumers/config/CMCConfig.java
+++
@@ -1,9 +0,0 @@
-package uk.gov.moj.sdt.cmc.consumers.config;
-
-import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@EnableFeignClients
-public class CMCConfig {
-}
