12d276225d3800ad288a093eafcaa77db15e62d3
Kiran Kumar Yenigala <51748133+kiran-yenigala-hmcts@users.noreply.github.com>
2023-06-26 16:31:06 +0100

    SDT-129: add commisioning module (#13)
    * SDT-129: move interceptors
    
    * SDT-129: move interceptors
    
    * SDT-129: move interceptors to common repo
    
    * SDT-129: move interceptors to common repo
    
    * Addressed reveiw comments

---
+++ interceptors/src/unit-test/java/uk/gov/moj/sdt/interceptors/out/ServiceRequestOutboundInterceptorTest.java
@@ -0,0 +1,91 @@
+package uk.gov.moj.sdt.interceptors.out;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+import org.apache.cxf.binding.soap.SoapMessage;
+import org.apache.cxf.io.CachedOutputStream;
+import org.apache.cxf.message.MessageImpl;
+import org.apache.cxf.phase.Phase;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import uk.gov.moj.sdt.domain.ServiceRequest;
+import uk.gov.moj.sdt.interceptors.service.IPersistServiceRequest;
+import uk.gov.moj.sdt.interceptors.service.RequestDaoService;
+import uk.gov.moj.sdt.utils.AbstractSdtUnitTestBase;
+import uk.gov.moj.sdt.utils.SdtContext;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+/**
+ * Test that faults are correctly intercepted.
+ *
+ * @author d195274
+ */
+@ExtendWith(MockitoExtension.class)
+class ServiceRequestOutboundInterceptorTest extends AbstractSdtUnitTestBase {
+
+    @Mock
+    IPersistServiceRequest persistServiceRequest;
+
+    RequestDaoService requestDaoService;
+
+    @Override
+    protected void setUpLocalTests() {
+        requestDaoService = new RequestDaoService(persistServiceRequest);
+    }
+
+    /**
+     * Test method for
+     * {@link ServiceRequestOutboundInterceptor
+     * #handleMessage(org.apache.cxf.binding.soap.SoapMessage)}.
+     */
+    @Test
+    void testHandleMessage() throws IOException {
+        SdtContext.getContext().setServiceRequestId(1L);
+        String data = "<xml>content</xml>";
+        final SoapMessage soapMessage = getDummySoapMessageWithCachedOutputStream(data);
+        final ServiceRequestOutboundInterceptor serviceRequestOutboundInterceptor = new ServiceRequestOutboundInterceptor(requestDaoService);
+        final ServiceRequest serviceRequest = new ServiceRequest();
+        when(persistServiceRequest.fetch(ServiceRequest.class, 1L)).thenReturn(serviceRequest);
+
+        assertNull(serviceRequest.getResponseDateTime());
+        assertNull(serviceRequest.getResponsePayload());
+        serviceRequestOutboundInterceptor.handleMessage(soapMessage);
+        assertNotNull(serviceRequest.getResponseDateTime());
+        String response = serviceRequest.getResponsePayload();
+        assertTrue(response.contains(data));
+        verify(persistServiceRequest).persist(serviceRequest);
+    }
+
+    @Test
+    void testHandleMessagePhaseConstructor() {
+        final ServiceRequestOutboundInterceptor serviceRequestOutboundInterceptor =
+            new ServiceRequestOutboundInterceptor(Phase.SETUP);
+
+        assertNotNull(serviceRequestOutboundInterceptor);
+    }
+
+    /**
+     * Builds a dummy soap message.
+     * <p>
+     * the path to the xml file containing the soap message.
+     *
+     * @return a soap message
+     */
+    private SoapMessage getDummySoapMessageWithCachedOutputStream(String data) throws IOException {
+        SoapMessage soapMessage = new SoapMessage(new MessageImpl());
+        try (CachedOutputStream cachedOutputStream = new CachedOutputStream()) {
+            cachedOutputStream.write(data.getBytes());
+            soapMessage.setContent(OutputStream.class, cachedOutputStream);
+        }
+        return soapMessage;
+    }
+
+}
