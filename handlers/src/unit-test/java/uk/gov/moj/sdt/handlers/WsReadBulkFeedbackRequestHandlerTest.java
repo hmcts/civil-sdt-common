b2ddb854d3a25445969dd1f2023682a486e646e2
ridoutp <paul.ridout@cgi.com>
2023-11-17 10:27:29 +0000

    SDT-181: Replace remaining JUnit4 references
    - Removed JUnit4 TestWatcher setup from AbstractIntegrationTest.  This only logged the start of a test which doesn't really offer any benefit.  The same code was removed from the AbstractSdtUnitTestBase class in an earlier pull request.
    - Replaced assertThrows() in AbstractDomainObjectVisitorTest with JUnit5 equivalent.  Changed tests so that they check message in returned exception - this was intended but not implemented correctly.
    - Changed HandlerTest classes to replace powermock with Mockito.
    - Removed powermock dependencies from build.gradle.  These included the last remaining JUnit4 dependency.
    - build.gradle had both plugin and non-plugin lombok configuration.  Only one of these is needed, so removed non-plugin lombok configuration.
    - Commented out empty suppress elements in suppressions.xml to prevent failure when dependency check is run.  Elements will be uncommented when suppressions need to be added to them.

--- handlers/src/unit-test/java/uk/gov/moj/sdt/handlers/WsReadBulkFeedbackRequestHandlerTest.java
+++ handlers/src/unit-test/java/uk/gov/moj/sdt/handlers/WsReadBulkFeedbackRequestHandlerTest.java
@@ -1,12 +1,10 @@
 package uk.gov.moj.sdt.handlers;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.MockedStatic;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.mockito.stubbing.Answer;
 import uk.gov.moj.sdt.domain.api.IBulkFeedbackRequest;
@@ -16,14 +14,19 @@ import uk.gov.moj.sdt.transformers.api.ITransformer;
 import uk.gov.moj.sdt.utils.mbeans.SdtMetricsMBean;
 import uk.gov.moj.sdt.utils.visitor.VisitableTreeWalker;
 import uk.gov.moj.sdt.validators.exception.CustomerNotFoundException;
+import uk.gov.moj.sdt.ws._2013.sdt.baseschema.ErrorType;
+import uk.gov.moj.sdt.ws._2013.sdt.baseschema.StatusCodeType;
+import uk.gov.moj.sdt.ws._2013.sdt.baseschema.StatusType;
 import uk.gov.moj.sdt.ws._2013.sdt.bulkfeedbackrequestschema.BulkFeedbackRequestType;
 import uk.gov.moj.sdt.ws._2013.sdt.bulkfeedbackrequestschema.HeaderType;
 import uk.gov.moj.sdt.ws._2013.sdt.bulkfeedbackresponseschema.BulkFeedbackResponseType;
-import uk.gov.moj.sdt.ws._2013.sdt.bulkfeedbackresponseschema.BulkRequestStatusType;
 
-import static org.mockito.ArgumentMatchers.any;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.mockStatic;
+import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.verify;
-import static org.powermock.api.mockito.PowerMockito.*;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class WsReadBulkFeedbackRequestHandlerTest {
@@ -53,7 +56,7 @@ class WsReadBulkFeedbackRequestHandlerTest {
 
     @BeforeEach
     public void setUp() {
-        wsReadBulkFeedbackRequestHandler = Mockito.spy(new WsReadBulkFeedbackRequestHandler(
+        wsReadBulkFeedbackRequestHandler = spy(new WsReadBulkFeedbackRequestHandler(
             mockBulkFeedbackService, mockTransformer));
         wsReadBulkFeedbackRequestHandler.setBulkFeedbackService(mockBulkFeedbackService);
         wsReadBulkFeedbackRequestHandler.setTransformer(mockTransformer);
@@ -73,38 +76,37 @@ class WsReadBulkFeedbackRequestHandlerTest {
 
         BulkFeedbackResponseType result;
 
-        try (MockedStatic<VisitableTreeWalker> mockStaticVisitableTreeWalker = Mockito.mockStatic(VisitableTreeWalker.class)) {
+        try (MockedStatic<VisitableTreeWalker> mockStaticVisitableTreeWalker = mockStatic(VisitableTreeWalker.class)) {
             mockStaticVisitableTreeWalker.when(() -> VisitableTreeWalker.walk(mockBulkFeedbackRequest, "Validator"))
                 .then((Answer<Void>) invocation -> null);
 
             result = wsReadBulkFeedbackRequestHandler.getBulkFeedback(mockBulkFeedbackRequestType);
 
             verify(mockTransformer).transformJaxbToDomain(mockBulkFeedbackRequestType);
-            verifyStatic(VisitableTreeWalker.class);
-            VisitableTreeWalker.walk(mockBulkFeedbackRequest, "Validator");
+            mockStaticVisitableTreeWalker.verify(() -> VisitableTreeWalker.walk(mockBulkFeedbackRequest, "Validator"));
             verify(mockBulkFeedbackService).getBulkFeedback(mockBulkFeedbackRequest);
             verify(mockTransformer).transformDomainToJaxb(mockBulkSubmission);
         }
 
         testMetrics();
-        Assertions.assertEquals(mockBulkFeedbackResponseType, result);
+        assertEquals(mockBulkFeedbackResponseType, result);
     }
 
     @Test
-    public void testGetBulkFeedbackThrowsAbstractBusinessException() throws Exception {
+    public void testGetBulkFeedbackThrowsAbstractBusinessException() {
         CustomerNotFoundException exception = new CustomerNotFoundException("MOCK_CODE", "MOCK_DESCRIPTION");
 
         when(mockTransformer.transformJaxbToDomain(mockBulkFeedbackRequestType))
             .thenThrow(exception);
 
-        wsReadBulkFeedbackRequestHandler.getBulkFeedback(mockBulkFeedbackRequestType);
+        BulkFeedbackResponseType bulkFeedbackResponseType =
+                wsReadBulkFeedbackRequestHandler.getBulkFeedback(mockBulkFeedbackRequestType);
 
-        verifyPrivate(wsReadBulkFeedbackRequestHandler)
-            .invoke(
-                "handleBusinessException",
-                any(CustomerNotFoundException.class),
-                any(BulkRequestStatusType.class)
-            );
+        StatusType statusType = bulkFeedbackResponseType.getBulkRequestStatus().getStatus();
+        assertEquals(StatusCodeType.ERROR, statusType.getCode(), "BulkFeedback response has unexpected status code");
+        ErrorType errorType = statusType.getError();
+        assertEquals("MOCK_CODE", errorType.getCode(), "Status error has unexpected code");
+        assertEquals("MOCK_DESCRIPTION", errorType.getDescription(), "Status error has unexpected description");
 
         testMetrics();
     }
@@ -112,7 +114,7 @@ class WsReadBulkFeedbackRequestHandlerTest {
     private void testMetrics() {
         String expectedBulkFeedbackCount = "count[1]";
 
-        Assertions.assertTrue(SdtMetricsMBean.getMetrics().getBulkFeedbackStats().contains(expectedBulkFeedbackCount));
-        Assertions.assertEquals(1, SdtMetricsMBean.getMetrics().getActiveBulkCustomers());
+        assertTrue(SdtMetricsMBean.getMetrics().getBulkFeedbackStats().contains(expectedBulkFeedbackCount));
+        assertEquals(1, SdtMetricsMBean.getMetrics().getActiveBulkCustomers());
     }
 }
