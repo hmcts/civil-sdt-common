f16013a13dff0d71a6a5f93bdc2cdb12d845efa1
ridoutp <paul.ridout@cgi.com>
2024-07-23 17:46:17 +0100

    SDT-201: Experimental - Added CMCUpdateRequest
    Added lombok.  Replaced getter and setter methods on CMCUpdateRequest with Getter and Setter annotations.

--- producers-api/src/main/java/uk/gov/moj/sdt/cmc/model/CMCUpdateRequest.java
+++ producers-api/src/main/java/uk/gov/moj/sdt/cmc/model/CMCUpdateRequest.java
@@ -1,27 +1,16 @@
 package uk.gov.moj.sdt.cmc.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Getter;
+import lombok.Setter;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
+@Getter
+@Setter
 public class CMCUpdateRequest {
 
     private int errorCode;
 
     private String errorText;
 
-    public int getErrorCode() {
-        return errorCode;
-    }
-
-    public String getErrorText() {
-        return errorText;
-    }
-
-    public void setErrorCode(int errorCode) {
-        this.errorCode = errorCode;
-    }
-
-    public void setErrorText(String errorText) {
-        this.errorText = errorText;
-    }
 }
