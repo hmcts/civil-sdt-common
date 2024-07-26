42bb9c674550c60c53dc763e1712f1d198453c57
ridoutp <paul.ridout@cgi.com>
2024-07-26 18:29:22 +0100

    SDT-201: Changed XML production to use JAXB
    - Added XmlEnum annotations to JudgmentWarrantStatus so that values of enum are used when converted to XML.
    - Updated CMCFeedbackTransformerTest to include tests for checking inclusion/exclusion of fields in XMl.

--- producers-api/src/main/java/uk/gov/moj/sdt/cmc/model/JudgmentWarrantStatus.java
+++ producers-api/src/main/java/uk/gov/moj/sdt/cmc/model/JudgmentWarrantStatus.java
@@ -2,15 +2,23 @@ package uk.gov.moj.sdt.cmc.model;
 
 import com.fasterxml.jackson.annotation.JsonValue;
 
+import javax.xml.bind.annotation.XmlEnum;
+import javax.xml.bind.annotation.XmlEnumValue;
+
+@XmlEnum
 public enum JudgmentWarrantStatus {
 
-    JUDGMENT_REQUEST_ERROR("Judgment Request error."),
-    WARRANT_REQUEST_ERROR("Warrant Request error."),
+    @XmlEnumValue("Judgment accepted by CCBC.")
     JUDGMENT_ACCEPTED_BY_CCBC("Judgment accepted by CCBC."),
+    @XmlEnumValue("Judgment accepted by CCBC.  Warrant accepted by CCBC.")
     JUDGMENT_ACCEPTED_WARRANT_ACCEPTED_BY_CCBC("Judgment accepted by CCBC.  Warrant accepted by CCBC."),
+    @XmlEnumValue("Judgment rejected by CCBC.  Warrant accepted by CCBC.")
     JUDGMENT_REJECTED_WARRANT_ACCEPTED_BY_CCBC("Judgment rejected by CCBC.  Warrant accepted by CCBC."),
+    @XmlEnumValue("Judgment rejected by CCBC.")
     JUDGMENT_REJECTED_BY_CCBC("Judgment rejected by CCBC."),
+    @XmlEnumValue("Judgment accepted by CCBC. Warrant rejected by CCBC.")
     JUDGMENT_ACCEPTED_WARRANT_REJECTED_BY_CCBC("Judgment accepted by CCBC. Warrant rejected by CCBC."),
+    @XmlEnumValue("Judgment rejected by CCBC.  Warrant rejected by CCBC.")
     JUDGMENT_REJECTED_WARRANT_REJECTED_BY_CCBC("Judgment rejected by CCBC.  Warrant rejected by CCBC.");
 
     private final String message;
